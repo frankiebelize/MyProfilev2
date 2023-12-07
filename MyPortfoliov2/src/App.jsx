d80f238aee8a44cb49086de543b0df8b9b19ce4d
frankiebelize <66229510+frankiebelize@users.noreply.github.com>
2023-12-07 01:30:12 -0600

    added alot of code

---
+++ MyPortfoliov2/src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from './componets/Navbar/Navbar'
+import styles from'./App.module.CSS'
+
+function App() {
+  
+
+  return <div className={styles.App}><Navbar/></div>
+    // <div className={styles.App}>
+    //   <Navbar/>
+    // </div>
+  
+}
+
+export default App
---
+++ MyPortfoliov2/src/componets/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import styles from "./navbar.module.CSS";
+import {getImageUrl} from "../../utils";
+
+export const Navbar = () => {
+    const {menuOpen, SetMenuOpen} = useState(false);
+    return ( 
+    <nav className={styles.navbar}>
+        <a className={styles.title} href="/">Portfolio</a>
+    <div className={styles.menu}>
+        <img className={styles.menuBtn} src={menuOpen ?
+        getImageUrl("nav/closeIcon.png") :
+        getImageUrl("nav/menuIcon.png")}
+        onClick={() => SetMenuOpen(!menuOpen)} ></img>
+       <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#skill">Skills</a>
+            </li>
+            <li>
+                <a href="#Projects">About</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+        </ul> 
+    </div>
+    </nav>
+    )
+};
